8eda69754c3b729394da600a99c4047e164d706d
RICARDO HUMBERTO FERRONATO NICOLLI <123266610+ricardogdz1@users.noreply.github.com>
2025-09-26 11:55:19 -0300

    Update urls.py

--- analisador/urls.py
+++ analisador/urls.py
@@ -6,4 +6,9 @@ app_name = 'analisador'
 urlpatterns = [
     path('', views.pagina_inicial, name='index'),
     path('processar/', views.processar_arquivo, name='processar'),
+    path(
+        "ads.txt",
+        TemplateView.as_view(template_name="ads.txt", content_type="text/plain"),
+        name="ads-txt",
+    ),
 ]
